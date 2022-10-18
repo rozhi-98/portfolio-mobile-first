e4c6d7eaee752a06be1b23f373c6f09bc96b8e9d
rozh bahman tahir <r.bahman98@gmail.com>
2022-10-18 18:58:50 +0300

    Add mobile menu with slide in and out functions

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const One = 3;
-const e = 'this is a letter';
+const navIcon = document.querySelector('.nav-icon');
+const navLinks = document.querySelector('.nav-links');
+// close menu when close button is pressed
+const closeBtn = document.querySelector('.close-btn');
 
+// open mobile menu
 
+navIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('show-menu');
+});
 
-const info = (One, e);
+closeBtn.addEventListener('click', () => {
+  if (navLinks.classList.contains('show-menu')) {
+    navLinks.classList.remove('show-menu');
+  }
+});
 
+// openning the correct menu option when clicked
 
-console.log(info);
+const listItem = document.querySelectorAll('.list-item');
+
+listItem.foreach((item) => {
+  item.addEventListener('click', (a) => {
+    const id = a.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    const position = element.offsetTop;
+
+    if (id) {
+      navLinks.classList.remove('show-menu');
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+  });
+});
