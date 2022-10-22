fc7d331d252825e1a1720b0ad893361abab57bcb
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-10-22 12:04:11 +0100

    set input data to local storage

--- app.js
+++ app.js
@@ -259,3 +259,39 @@ form.addEventListener('submit', (e) => {
     alert.textContent = 'email has to be in lowercase format';
   }
 });
+
+// local storage
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException
+      && (e.code === 22
+        || e.code === 1014
+        || e.name === 'QuotaExceededError'
+        || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      && storage
+      && storage.length !== 0
+    );
+  }
+}
+
+function test() {
+  const Inputname = document.getElementById('username').value;
+  const Inputemail = document.getElementById('email').value;
+  const Inputmessage = document.getElementById('text-description').value;
+
+  localStorage.setItem('Inputname', Inputname);
+  localStorage.setItem('Inputemail', Inputemail);
+  localStorage.setItem('Inputmessage', Inputmessage);
+}
+
+const submit = document.querySelector('submit-btn');
+submit.addEventListener('submit', test());
